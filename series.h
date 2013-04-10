78d105c64a4a6f8aeec00893e31179d11cdb87ea
Martin Ueding <dev@martin-ueding.de>
2013-04-10 11:45:14 +0200

    Documentation

--- series.h
+++ series.h
@@ -5,9 +5,36 @@
 
 #include <math.h>
 
+/**
+ * Calculates pi using the series a.
+ *
+ * @param k Number of summands to consider
+ * @return Approximated value of pi
+ */
 double pi_a(int k);
+
+/**
+ * Calculates pi using the series b.
+ *
+ * @param k Number of summands to consider
+ * @return Approximated value of pi
+ */
 double pi_b(int k);
+
+/**
+ * Calculates the value of the series in equation (3)
+ *
+ * @param k Number of summands to consider
+ * @return Approximated value of series
+ */
 double series_a(int k);
+
+/**
+ * Calculates the value of the series in equation (4)
+ *
+ * @param k Number of summands to consider
+ * @return Approximated value of series
+ */
 double series_b(int k);
 
 #endif /* end of include guard: SERIES_H */
