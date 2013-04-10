64b55ca830c2172beeda576817a1446442148db0
Martin Ueding <dev@martin-ueding.de>
2013-04-10 10:55:12 +0200

    Correct step size

--- 2.c
+++ 2.c
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
 	char filename[] = "sin.csv";
 	FILE *fp = fopen(filename, "w");
 
-	for (double x = -20.0; x <= 20.0; ++x) {
+	for (double x = -20.0; x <= 20.0; x += 0.05) {
 		fprintf(fp, "%f\t%f\n", x, my_abs(sin(x) + 0.2));
 	}
 
