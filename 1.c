f5013d76633c78000eb86e5353ee2afc721a9636
Martin Ueding <dev@martin-ueding.de>
2013-04-17 09:34:33 +0200

    Crank up precision

--- 1.c
+++ 1.c
@@ -15,7 +15,7 @@ double f(double x) {
 }
 
 int main(int argc, char **argv) {
-	double root = bisect(0.0, 5.0, f, 100, 0.0001);
+	double root = bisect(0.0, 5.0, f, 100, 1.0e-10);
 	printf("Root: %g\n", root);
 
 	return 0;
