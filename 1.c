2710b5eff3618e73e98d9e2cffb5ee7c3c971f55
Martin Ueding <dev@martin-ueding.de>
2013-04-17 10:23:08 +0200

    Compute both roots

--- 1.c
+++ 1.c
@@ -15,8 +15,9 @@ double f(double x) {
 }
 
 int main(int argc, char **argv) {
-	double root = bisect(0.0, 5.0, f, 10000, 1.0e-10);
-	printf("Root: %g\n", root);
+	double root1 = bisect(-5.0, 0.0, f, 10000, 1.0e-10);
+	double root2 = bisect(0.0, 5.0, f, 10000, 1.0e-10);
+	printf("Roots: %g, %g\n", root1, root2);
 
 	return 0;
 }
