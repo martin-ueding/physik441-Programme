7cf9828f32295434e049e369072493ca54ed566f
Martin Ueding <dev@martin-ueding.de>
2013-04-17 08:44:06 +0200

    Main program

---
+++ 1.c
@@ -0,0 +1,17 @@
+// Copyright © 2013 Martin Ueding <dev@martin-ueding.de>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Test function.
+ */
+double f(double x) {
+	return x*x - x - 2;
+}
+
+int main(int argc, char **argv) {
+	double (*function)(double);
+
+	return 0;
+}
