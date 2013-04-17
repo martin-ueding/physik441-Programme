3823c974388d08a44c2f7a5fd74a527268561895
Martin Ueding <dev@martin-ueding.de>
2013-04-17 08:45:32 +0200

    Method stub

---
+++ bisect.c
@@ -0,0 +1,7 @@
+// Copyright © 2013 Martin Ueding <dev@martin-ueding.de>
+
+#include "bisect.h"
+
+double bisect(double a1, double a2, double (*f)(double), int max_iter, double precision) {
+	return 0.0
+}
