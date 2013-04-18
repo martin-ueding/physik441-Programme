9ef50fda6fb856927a0b6aeeaec4c8f4eb3a0ffb
Martin Ueding <dev@martin-ueding.de>
2013-04-18 22:58:46 +0200

    Namespace

--- bisect.hpp
+++ bisect.hpp
@@ -9,26 +9,24 @@
 #include <iostream>
 
 /**
- * “Bisect” root finding algorithmn and helper functions.
+ “Bisect” root finding algorithmn and helper functions.
  */
-class Bisect {
-	public:
-		/**
-		 * Finds a root of the given function.
-		 *
-		 * @param a1 Minimal start value
-		 * @param a2 Maximal start value
-		 * @param f Function to use
-		 * @param max_iter Maximal number of iterations
-		 * @param precision Working precision
-		 */
-		static double bisect(double a1, double a2, double(*f)(double), int max_iter, double precision);
+namespace bisect {
+	/**
+	 Finds a root of the given function.
 
-	private:
-		/**
-		 * Checks whether the arguments have opposite sign.
-		 */
-		static int opposite_signs(double x1, double x2);
-};
+	 @param a1 Minimal start value
+	 @param a2 Maximal start value
+	 @param f Function to use
+	 @param max_iter Maximal number of iterations
+	 @param precision Working precision
+	 */
+	double bisect(double a1, double a2, double(*f)(double), int max_iter, double precision);
+
+	/**
+	 Checks whether the arguments have opposite sign.
+	 */
+	int opposite_signs(double x1, double x2);
+}
 
 #endif /* end of include guard: BISECT_H */
--- main.cpp
+++ main.cpp
@@ -11,41 +11,41 @@
  @author Jan Weber
 
  @section compile Kompilieren und Ausführen
- 
+
  Mit <tt>make dep && make</tt> wird alles kombiliert und ausgeführt, die
  Ausgaben erscheinen ganz unten in der Ausgabe von <tt>make</tt>:
- 
+
      Ausgabe Programm
      ./main
      Roots -12
      U_d = 5.95465
      I_d = 1
- 
+
  @section Abhängigkeiten
- 
+
  - g++
  - make
- 
+
  @section Überlegungen zu Aufgabe 2
- 
+
  Der Strom, der durch die Diode geht ist:
- 
+
  @f[
      I_D(U_D) = I_S \exp\left( \frac{U_D}{m U_T} \right)
  @f]
- 
+
  Die Spannung, die über dem Widerstand und der Diode abfällt, muss gerade die
  Eingangsspannung sein:
- 
+
  @f[
      U_0 - U_D + R I_D (U_D) = 0
  @f]
  */
 
 /**
- * Test function.
- *
- * It has roots at x=-1 and x=2.
+ Test function.
+
+ It has roots at x=-1 and x=2.
  */
 double parabola(double x) {
 	return x * x - x - 2;
@@ -67,14 +67,14 @@ double f(double u_d) {
 }
 
 int aufgabe_a() {
-	double root1 = Bisect::bisect(-5.0, 0.0, parabola, 10000, 1.0e-10);
-	double root2 = Bisect::bisect(0.0, 5.0, parabola, 10000, 1.0e-10);
+	double root1 = bisect::bisect(-5.0, 0.0, parabola, 10000, 1.0e-10);
+	double root2 = bisect::bisect(0.0, 5.0, parabola, 10000, 1.0e-10);
 	std::cout << "Roots " << root1 << root2 << "\n";
 	return 0;
 }
 
 int aufgabe_b() {
-	double u_d = Bisect::bisect(0.0, 100.0, f, 1000000, 1e-6);
+	double u_d = bisect::bisect(0.0, 100.0, f, 1000000, 1e-6);
 	double i_d_ = i_d(u_d);
 	std::cout << "U_d = " << u_d << std::endl;
 	std::cout << "I_d = " << i_d << std::endl;
