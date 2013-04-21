3850f741a07eadad7b09f840e9242844e79776a4
Martin Ueding <dev@martin-ueding.de>
2013-04-21 19:15:35 +0200

    Add exception test

--- test_BisectTest.cpp
+++ test_BisectTest.cpp
@@ -2,6 +2,14 @@
 
 #include "test_BisectTest.hpp"
 
+double BisectTest::one(double x) {
+	return 1;
+}
+
+void BisectTest::testException() {
+	CPPUNIT_ASSERT(bisect::bisect(-1., 1., BisectTest::one, 5, 0.1));
+}
+
 void BisectTest::testOppositeSigns() {
 	CPPUNIT_ASSERT(bisect::opposite_signs(-1., 1.));
 }
--- test_BisectTest.hpp
+++ test_BisectTest.hpp
@@ -12,6 +12,7 @@
  */
 class BisectTest: public CppUnit::TestFixture {
 	CPPUNIT_TEST_SUITE(BisectTest);
+	CPPUNIT_TEST_EXCEPTION(testException, bisect::NoRootException);
 	CPPUNIT_TEST(testOppositeSigns);
 	CPPUNIT_TEST_SUITE_END();
 
@@ -20,6 +21,20 @@ class BisectTest: public CppUnit::TestFixture {
 		 Tests the oppsoite_signs function.
 		 */
 		void testOppositeSigns();
+
+		/**
+		 Checks for the exception.
+		 */
+		void testException();
+
+	private:
+		/**
+		 Test function that always gives one.
+
+		 @param x Input value.
+		 @returns Always 1.0.
+		 */
+		static double one(double x);
 };
 
 #endif /* end of include guard: BISECTTEST_H */
