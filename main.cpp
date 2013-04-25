19700ab020f36cba60923ff313d3b657e35b4f19
Martin Ueding <dev@martin-ueding.de>
2013-04-25 16:30:14 +0200

    Documentation

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 
  Wie immer, mit <tt>make dep && make -j4</tt> werden alle Programmteile
  kompiliert und ausgeführt. Mit <tt>make dep && make -j4 test</tt> können alle
- Tests ausgeführt werden, die benötigt die CppUnit Bibliothek.
+ Tests ausgeführt werden, diese benötigen die CppUnit Bibliothek.
 
  @section aufgaben Aufgaben
 
@@ -24,6 +24,13 @@
 
  Das Programm liefert für den Startwert 0 ab einem gewissen Iterationsindex die
  Werte -5 und 0.5 im Wechsel. Nach 30 Iterationen ist meist 0.5 erreicht.
+
+ @subsection aufgabe4 Aufgabe 4
+
+ Das zu bestimmende Integral ist:
+ @f[
+	\int_{-2}^1 \mathrm dx \, \left( x^3 + 2 x^2 + \frac 12 x + 2 \right) = 7.5
+ @f]
  */
 
 #include <iostream>
