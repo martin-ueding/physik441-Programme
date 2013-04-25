55ce99e939d06831362e2a87d899ca07109b0100
Martin Ueding <dev@martin-ueding.de>
2013-04-25 13:16:43 +0200

    Comments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 // Copyright © 2013 Martin Ueding <dev@martin-ueding.de>
 
+/**
+ @file
+ @mainpage physik441 – Übung 3
+ @author Martin Ueding <dev@martin-ueding.de>
+ @author Jan Weber
+
+ @section aufgaben Aufgaben
+ @subsection aufgabe1 Aufgabe 1
+
+ Die Fixpunkte der Funktion @f$ f(x) = x @f$ liegen bei:
+ */
+
 #include <iostream>
 
+#include "aufgaben.hpp"
+
 int main() {
+	aufgabe1();
 
 	return 0;
 }
