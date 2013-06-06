91c55e2f0b192630113cae1806cb7c3a1edb64b1
Martin Ueding <dev@martin-ueding.de>
2013-06-06 14:51:39 +0200

    Implement more

--- splines.c
+++ splines.c
@@ -16,42 +16,60 @@ double cubic_spline_eval(double x, double x0, double *coefficients) {
 }
 
 void cubic_spline_interpolate(double *x, double *y, int data_count, double **coefficients) {
-	double *a;
-	double *b;
-	double *c;
-	double *d;
+	double *A;
+	double *B;
+	double *C;
+	double *D;
 	double *h;
 	double *y_xx;
+	double *X;
    
-	a = malloc(sizeof(*a) * data_count);
-	b = malloc(sizeof(*b) * data_count);
-	c = malloc(sizeof(*c) * data_count);
-	d = malloc(sizeof(*d) * data_count);
+	A = malloc(sizeof(*A) * data_count);
+	B = malloc(sizeof(*B) * data_count);
+	C = malloc(sizeof(*C) * data_count);
+	D = malloc(sizeof(*D) * data_count);
 	h = malloc(sizeof(*h) * data_count);
 	y_xx = malloc(sizeof(*y_xx) * data_count);
 
-	assert(a);
-	assert(b);
-	assert(c);
-	assert(d);
+	assert(A);
+	assert(B);
+	assert(C);
+	assert(D);
 	assert(h);
 	assert(y_xx);
 
-	for (int i = 0; i < N-1; i++) {
+	// Calculate h.
+	for (int i = 0; i < data_count-1; i++) {
 		h[i] = x[i+1] - x[i];
 	}
 
-	for (int i = 0; i < N-1; i++) {
-		a[i] = 1./(6. * h[i]) * (y_xx[i+1] - y_xx[i]);
-		b[i] = 1./2. * y_xx[i];
-		c[i] = 1./h[i] * (y[i+1] - y[1]) - 1./6. * h[i] * (y_xx[i+1] + 2. * y[i]);
-		d[i] = y[i];
+	// Calculate A, B, C, D.
+	for (int i = 1; i < data_count-1; i++)
+		A[i] = h[i-1];
+	for (int i = 0; i < data_count; i++)
+		B[i] = 2. * (h[i-1] + h[i]);
+	for (int i = 0; i < data_count-2; i++)
+		C[i] = h[i];
+	for (int i = 0; i < data_count-1; i++)
+		D[i] = 6./h[i] * (y[i+1] - y[i]) - 6./h[i-1] * (y[i] - y[i-1]);
+
+	// Calculate the X.
+	for (int i = 0; i > 0; i--) {
+		X[i] = D2[i]/B2[i]-C2[i]/B2[i] * X[i+1];
 	}
 
-	free(a);
-	free(b);
-	free(c);
-	free(d);
+	// Calculate the final coefficients.
+	for (int i = 0; i < data_count-1; i++) {
+		coefficients[i][0] = 1./(6. * h[i]) * (y_xx[i+1] - y_xx[i]);
+		coefficients[i][1] = 1./2. * y_xx[i];
+		coefficients[i][2] = 1./h[i] * (y[i+1] - y[1]) - 1./6. * h[i] * (y_xx[i+1] + 2. * y[i]);
+		coefficients[i][3] = y[i];
+	}
+
+	free(A);
+	free(B);
+	free(C);
+	free(D);
 	free(h);
 	free(y_xx);
 }
