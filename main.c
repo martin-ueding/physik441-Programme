258a4e30c056cef725e5caf8cdf87c87f7b73309
Martin Ueding <dev@martin-ueding.de>
2013-06-13 22:17:06 +0200

    Run astyle

--- main.c
+++ main.c
@@ -18,36 +18,36 @@ double dot_omega_simple(double t, double phi, double omega) {
 }
 
 void runge_kutta_2d_step(double t, double *phi, double *omega, double h,
-		double(*dot_phi)(double, double, double),
-		double(*dot_omega)(double, double, double)) {
+        double(*dot_phi)(double, double, double),
+        double(*dot_omega)(double, double, double)) {
 	double phi_new, omega_new;
 
 	static double k[4][2];
 	k[0][0] = dot_phi(t, *phi, *omega);
 	k[0][1] = dot_omega(t, *phi, *omega);
-	phi_new = *phi + k[0][0] * h/2.;
-	omega_new = *omega + k[0][1] * h/2.;
+	phi_new = *phi + k[0][0] * h / 2.;
+	omega_new = *omega + k[0][1] * h / 2.;
 
-	k[1][0] = dot_phi(t + h/2., phi_new, omega_new);
-	k[1][1] = dot_omega(t + h/2., phi_new, omega_new);
-	phi_new = *phi + k[1][0] * h/2.;
-	omega_new = *omega + k[1][1] * h/2.;
+	k[1][0] = dot_phi(t + h / 2., phi_new, omega_new);
+	k[1][1] = dot_omega(t + h / 2., phi_new, omega_new);
+	phi_new = *phi + k[1][0] * h / 2.;
+	omega_new = *omega + k[1][1] * h / 2.;
 
-	k[2][0] = dot_phi(t + h/2., phi_new, omega_new);
-	k[2][1] = dot_omega(t + h/2., phi_new, omega_new);
-	phi_new = *phi + k[2][0] * h/2.;
-	omega_new = *omega + k[2][1] * h/2.;
+	k[2][0] = dot_phi(t + h / 2., phi_new, omega_new);
+	k[2][1] = dot_omega(t + h / 2., phi_new, omega_new);
+	phi_new = *phi + k[2][0] * h / 2.;
+	omega_new = *omega + k[2][1] * h / 2.;
 
-	k[3][0] = dot_phi(t + h/2., phi_new, omega_new);
-	k[3][1] = dot_omega(t + h/2., phi_new, omega_new);
+	k[3][0] = dot_phi(t + h / 2., phi_new, omega_new);
+	k[3][1] = dot_omega(t + h / 2., phi_new, omega_new);
 
-	*phi += (k[0][0] + 2.*k[1][0] + 2.*k[2][0] + k[3][0])* h/6.;
-	*omega += (k[0][1] + 2.*k[1][1] + 2.*k[2][1] + k[3][1])* h/6.;
+	*phi += (k[0][0] + 2.*k[1][0] + 2.*k[2][0] + k[3][0]) * h / 6.;
+	*omega += (k[0][1] + 2.*k[1][1] + 2.*k[2][1] + k[3][1]) * h / 6.;
 }
 
 void runge_kutta_2d(double omega, char *filename,
-		double(*dot_phi)(double, double, double),
-		double(*dot_omega)(double, double, double)) {
+        double(*dot_phi)(double, double, double),
+        double(*dot_omega)(double, double, double)) {
 	double h = 0.1;
 	double t = 0.;
 	double phi = 0;
@@ -64,8 +64,8 @@ void runge_kutta_2d(double omega, char *filename,
 }
 
 void runge_kutta_2d_tolerance(double omega, char *filename,
-		double(*dot_phi)(double, double, double),
-		double(*dot_omega)(double, double, double), double tolerance) {
+        double(*dot_phi)(double, double, double),
+        double(*dot_omega)(double, double, double), double tolerance) {
 	double h = 0.1;
 	double t = 0.;
 	double phi = 0.;
@@ -79,16 +79,17 @@ void runge_kutta_2d_tolerance(double omega, char *filename,
 	assert(fp);
 	for (t = 0.; t <= 20.; t += h) {
 		do {
-		phi_half = phi;
-		omega_half = omega;
-		runge_kutta_2d_step(t, &phi, &omega, h, dot_phi, dot_omega);
-		runge_kutta_2d_step(t, &phi_half, &omega_half, h/2., dot_phi, dot_omega);
-		d_phi = fabs((phi - phi_half)/(1-1./16));
-		d_omega = fabs((omega - omega)/(1-1./16));
-		d = d_phi > d_omega ? d_phi : d_omega;
-		h = .8 * h * pow(tolerance/d, 1./5.);
-		} while (d > tolerance);
-		fprintf(fp, "%g\t%g\t%g\t%g\n", t, phi, omega, h*2);
+			phi_half = phi;
+			omega_half = omega;
+			runge_kutta_2d_step(t, &phi, &omega, h, dot_phi, dot_omega);
+			runge_kutta_2d_step(t, &phi_half, &omega_half, h / 2., dot_phi, dot_omega);
+			d_phi = fabs((phi - phi_half) / (1 - 1. / 16));
+			d_omega = fabs((omega - omega) / (1 - 1. / 16));
+			d = d_phi > d_omega ? d_phi : d_omega;
+			h = .8 * h * pow(tolerance / d, 1. / 5.);
+		}
+		while (d > tolerance);
+		fprintf(fp, "%g\t%g\t%g\t%g\n", t, phi, omega, h * 2);
 		steps++;
 	}
 	fclose(fp);
